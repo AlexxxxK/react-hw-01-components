0c1b79d19a3a6e4c7ea0fcf51ff526184bb932b9
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-09-16 08:35:45 +0300

    Updates

---
+++ precache-manifest.f369cf8b73c4b8a8bae3a7c0676981b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7019fbc4628e45ac3c39dc334f5f3fe0",
+    "url": "/react-hw-01-components-pricing-plan/index.html"
+  },
+  {
+    "revision": "b82460a400597111b539",
+    "url": "/react-hw-01-components-pricing-plan/static/css/main.68eb2de5.chunk.css"
+  },
+  {
+    "revision": "be3012b57cc8460b9d4d",
+    "url": "/react-hw-01-components-pricing-plan/static/js/2.74114385.chunk.js"
+  },
+  {
+    "revision": "b82460a400597111b539",
+    "url": "/react-hw-01-components-pricing-plan/static/js/main.868142ef.chunk.js"
+  },
+  {
+    "revision": "719d27e1b6f5cccde81e",
+    "url": "/react-hw-01-components-pricing-plan/static/js/runtime~main.3375ac80.js"
+  }
+]);
